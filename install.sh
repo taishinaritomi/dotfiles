065a9cc4067caa0b48062bfe7f1ceebb6a4808c9
taishinaritomi <taishinaritomi@gmail.com>
2023-12-03 17:02:28 +0900

    update install command

--- install.sh
+++ install.sh
@@ -15,14 +15,23 @@ fi
 
 find ~/dotfiles &>/dev/null || git clone git@github.com:taishinaritomi/dotfiles.git
 
-find ~/.config &>/dev/null || cd ~ && mkdir -p .config
 
 brew bundle -v --file=~/dotfiles/packages/brew/.Brewfile
+
+find ~/.config &>/dev/null || cd ~ && mkdir -p .config
 
 for file in ~/.config/config.fish  ~/.gitconfig ~/.config/starship.toml ~/.Brewfile ~/.tool-versions ; do
   rm -rf "${file}"
 done
 stow -v -d ~/dotfiles/packages -t ~ fish git starship brew kitty asdf
+
+VSCODE_USER_DIR="$HOME/Library/Application Support/Code/User"
+find "$VSCODE_USER_DIR" &>/dev/null || mkdir -p "$VSCODE_USER_DIR"
+rm -rf "$VSCODE_USER_DIR/settings.json"
+rm -rf "$VSCODE_USER_DIR/keybindings.json"
+rm -rf "$VSCODE_USER_DIR/snippets"
+stow -v -d ~/dotfiles/packages -t "$VSCODE_USER_DIR" vscode
+
 
 if [[ $(uname -m) == "arm64" ]]; then
     FISH_PATH="/opt/homebrew/bin/fish"
@@ -35,9 +44,3 @@ grep -c -q $FISH_PATH /etc/shells &>/dev/null || {
 }
 
 chsh -s $FISH_PATH
-
-find ~/Library/Application\ Support/Code/User &>/dev/null || cd ~ && mkdir -p ~/Library/Application\ Support/Code/User
-rm -rf ~/Library/Application\ Support/Code/User/settings.json
-rm -rf ~/Library/Application\ Support/Code/User/keybindings.json
-rm -rf ~/Library/Application\ Support/Code/User/snippets
-stow -v -d ~/dotfiles/packages -t ~/Library/Application\ Support/Code/User/ vscode
